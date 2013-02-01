f452c3f76153c8e1d8a60d7376da369a61988d48
zekai.zheng <kiddkai@gmail.com>
2013-02-01 19:26:12 +0800

    no regular expression & remove push interface & add pushSync & pushAsync interface... code is simple

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -23,57 +23,104 @@ function Queue() {
     return '[object Function]' === toString.call(obj)
   }
 
-  function push(fn, delayMillSec, context) {
-    if (delayMillSec) {
-      delay(delayMillSec, context)
-    }
 
-    if (context) {
-      push(function() {
-        var args = toArray.call(arguments, 0)
-        return fn.apply(context, args)
-      })
+  function Executor(fnType, fn, context) {
+    if (this instanceof Executor) {
+      var self = this
+      this.fnType = fnType
+      this.context = context
+      this.fn = fn
+      if (this.context) {
+        this.fn = function() {
+          fn.apply(self.context, arguments)
+        }
+      }
     }
     else {
-      fnQueue.push(fn)
+      return new Executor(fnType, fn, context)
     }
+  }
+
+
+  Executor.prototype.isSync = function() {
+    return this.fnType === 'sync'
+  }
+
+  function push(executer, delayMillSec) {
+
+    if (delayMillSec) {
+      delay(delayMillSec, executer.context)
+    }
+
+    fnQueue.push(executer)
 
     return this
   }
 
+  /**
+   * 把同步方法push进来
+   * @param fn
+   * @param delayMillSec
+   * @param context
+   */
+  function pushSync(fn, delayMillSec, context) {
+    var executor = Executor('sync', fn, context)
+    push(executor, delayMillSec)
+    return this
+  }
+
+  /**
+   * 把异步方法push进来
+   * @param fn
+   * @param delayMillSec
+   * @param context
+   */
+  function pushAsync(fn, delayMillSec, context) {
+    var executor = Executor('async', fn, context)
+    push(executor, delayMillSec)
+    return this
+  }
 
   function delay(milliSec, context) {
-    fnQueue.push(function(){
+    var delayFunc = function() {
       var args = toArray.call(arguments, 0)
+        , next = args[args.length - 1]
+      args = args.slice(0, args.length - 1)
+
       window.setTimeout(function() {
-        next.apply(context ? context : this , args)
+        next.apply(context ? context : this, args)
       }, milliSec)
-    })
+    }
+
+    var executer = Executor('async', delayFunc, context)
+    push(executer, 0)
+
     return this
   }
 
 
   function next() {
     var args = toArray.call(arguments, 0)
-      , fn = fnQueue.shift()
+      , executor = fnQueue.shift()
+      , fn = null
       , returnVal = null
-      , NEXT_REGEXP = new RegExp("[\\{|;|\\n]+\\s*" + next.name + "(\\.call|\\.apply)?\\([^\\)]*\\)")
 
-    if (!fn) {
+    if (!executor) {
       return
     }
+
+    fn = executor.fn
 
     args.push(next);
     returnVal = fn.apply(this, args)
 
-    if (returnVal) {
-      next(returnVal)
-    }
-    else {
-      if(!NEXT_REGEXP.test(fn.toString())) {
-        next();
+    if (executor.isSync()) {
+      if (returnVal) {
+        next(returnVal)
       }
-
+      else {
+        next()
+      }
     }
   }
 
@@ -83,7 +130,9 @@ function Queue() {
 
   return {
 
-      push: push
+      pushSync: pushSync
+
+    , pushAsync: pushAsync
 
     , delay: delay
 

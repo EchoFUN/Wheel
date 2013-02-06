15b45a1ddc7e7141992a14d8dd0a81d5bf0b0a62
zekai.zheng <kiddkai@gmail.com>
2013-02-06 09:33:12 +0800

    modified test suite

--- test/ui-date-picker/unit/date-util.js
+++ test/ui-date-picker/unit/date-util.js
@@ -1,21 +1,21 @@
 describe('date utils test suit', function () {
   it('should be have 31 days at January', function() {
-    expect(31).toBe(daysOfMonth(1));
+    expect(31).toBe(daysOfMonth(0));
   });
 
   it('should be have 28 days at February', function() {
-    expect(28).toBe(daysOfMonth(2));
+    expect(28).toBe(daysOfMonth(1));
   });
 
   it('should be have 29 days in a leap year at February', function() {
-    expect(29).toBe(daysOfMonth(2012, 2));
+    expect(29).toBe(daysOfMonth(2012, 1));
   });
 
   it('should be have 31 days at March', function() {
-    expect(31).toBe(daysOfMonth(3));
+    expect(31).toBe(daysOfMonth(2));
   });
 
   it('should be have 30 days at April', function() {
-    expect(30).toBe(daysOfMonth(4));
+    expect(30).toBe(daysOfMonth(3));
   });
 });
